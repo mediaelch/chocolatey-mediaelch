7500a607ac386cf7174e640d107dad57fe2bfe0e
Michael Sumerano <msumerano@gmail.com>
2021-04-28 09:37:38 -0400

    Updated to v2.8.8 #9

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@ $ErrorActionPreference = 'Stop'
 
 $packageArgs = @{
   packageName    = 'MediaElch'
-  url            = 'https://github.com/Komet/MediaElch/releases/download/v2.8.6/MediaElch_win_2.8.6_2021-01-22_git-f215c8a6.zip'
-  checksum       = 'E2183EB091114477C13DCDDF473356B47DC364CF565792D32C95DAA1FFB9564A'
+  url            = 'https://github.com/Komet/MediaElch/releases/download/v2.8.8/MediaElch_win_2.8.8_2021-04-27_git-14260f64.zip'
+  checksum       = '7BE44F81F40712B4B483EC8BA2726916D0F99F96F1BC713E02A2D70B605B78D8'
   checksumType   = 'sha256'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 }
